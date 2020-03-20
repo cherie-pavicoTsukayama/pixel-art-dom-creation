8a94d86802cba8b3a85b8785084d84f35fb843d7
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-03-19 22:25:32 -0700

    completed 2 rows of the red, still need to do one more."

--- main.js
+++ main.js
@@ -4,13 +4,14 @@ var body = document.body;
 var div = document.createElement('div');
 
 var colorArrayId = ['redSection', 'orangeSection', 'yellowSection', 'greenSection', 'blueSection', 'purpleSection'];
-var rowsArray = ['row1', 'row2', 'row3']
+var rowsArray = ['row1', 'row2', 'row3'];
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
 
 function createSections(array){
     for(var i = 0; i < array.length; i++){
         var section = document.createElement('section');
         section.setAttribute('id', array[i]);
-        section.setAttribute('class', 'container' +' height' + ' col-12');
+        section.setAttribute('class', 'container' +' height' + ' col-15');
         body.appendChild(section);
     }
 }
@@ -22,7 +23,8 @@ function createRows(array){
         var section = colorSection[i];
         for(var row = 0; row < rowsArray.length; row++){
             var rowDiv = document.createElement('div');
-            rowDiv.setAttribute('id', rowsArray[row]);
+            rowDiv.setAttribute('id', colors[i]+ rowsArray[row]);
+            rowDiv.setAttribute('class', 'col-15 row-container');
             section.appendChild(rowDiv);
         }
 
@@ -30,3 +32,20 @@ function createRows(array){
 }
 
 createRows(colorSection);
+
+var redRow1 = document.getElementById('redrow1');
+redRow1.appendChild(div).setAttribute('class', 'white col-3 block-height');
+redRow1.appendChild(document.createElement('div')).setAttribute('class', 'black block-height col-4');
+redRow1.appendChild(document.createElement('div')).setAttribute('class', 'white block-height col-3');
+redRow1.appendChild(document.createElement('div')).setAttribute('class', 'black block-height col-4');
+redRow1.appendChild(document.createElement('div')).setAttribute('class', 'white block-height col-4');
+var redRow2 = document.getElementById('redrow2');
+redRow2.appendChild(document.createElement('div')).setAttribute('class', 'white col-2 block-height');
+redRow2.appendChild(document.createElement('div')).setAttribute('class', 'black col-1 block-height');
+redRow2.appendChild(document.createElement('div')).setAttribute('class', 'red col-4 block-height');
+redRow2.appendChild(document.createElement('div')).setAttribute('class', 'black col-1 block-height');
+redRow2.appendChild(document.createElement('div')).setAttribute('class', 'white col-1 block-height');
+redRow2.appendChild(document.createElement('div')).setAttribute('class', 'black col-1 block-height');
+redRow2.appendChild(document.createElement('div')).setAttribute('class', 'red col-4 block-height');
+redRow2.appendChild(document.createElement('div')).setAttribute('class', 'black col-1 block-height');
+redRow2.appendChild(document.createElement('div')).setAttribute('class', 'white col-2 block-height');
